99d35363799e4e55989411eb5bd0be66112c34ba
kenkode <wangoken2@gmail.com>
2019-02-11 17:34:21 +0300

    changes

--- src/public/index.php
+++ src/public/index.php
@@ -42,57 +42,66 @@ $container['db'] = function ($c) {
 $app->group('/api', function () use ($app) {
 	$app->get('/available-buses', 'getAvailableBuses');
 	$app->post('/users', 'postUsers');
-	$app->get('/stations', 'getStations');
 	$app->post('/boardings', 'insertBoardings');
 });
 function getConnection() {
     $dbhost="127.0.0.1";
     $dbuser="root";
-    $dbpass="ken2017";
+    $dbpass="";
     $dbname="altruistic";
     $dbh = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     return $dbh;
 }
-function getAvailableBuses($response) {
-    $sql = "select * FROM bus";
+function getAvailableBuses($request) {
+	$cordinates = json_decode($request->getBody());
+	
+	$stationSql = "SELECT id, ( 6371 * acos( cos( radians( " . $cordinates->lat . " ) ) * cos( radians( latitude ) ) * 
+			cos( radians( longitude ) - radians( " . $cordinates->lng . ") ) + sin( radians( " . $cordinates->lat . " ) ) * 
+			sin( radians( latitude ) ) ) ) AS distance FROM stations HAVING
+			distance < 0.05 ORDER BY distance LIMIT 1";
     try {
-        $stmt = getConnection()->query($sql);
-        $buses = $stmt->fetchAll(PDO::FETCH_OBJ);
-        $db = null;
+		$db = getConnection();
+        $stmt = $db->query($stationSql);
+        $station = $stmt->rowCount();
+		
+		$buses = [];
+		
+		if($station > 0){
+			$sql = "select * FROM bus";
+			$stmt = getConnection()->query($sql);
+			$buses = $stmt->fetchAll(PDO::FETCH_OBJ);
+			$db = null;
+		}
 		
         return json_encode($buses);
     } catch(PDOException $e) {
         echo '{"error":{"text":'. $e->getMessage() .'}}';
-    }
-}
-function getStations($response) {
-    $sql = "select * FROM stations";
-    try {
-        $stmt = getConnection()->query($sql);
-        $stations = $stmt->fetchAll(PDO::FETCH_OBJ);
-        $db = null;
-		
-        return json_encode($stations);
-    } catch(PDOException $e) {
-        echo '{"error":{"text":'. $e->getMessage() .'}}';
-    }
+    } 
 }
 function postUsers($request) {
     $user = json_decode($request->getBody());
 	
-    $sql = "INSERT INTO users (station_id, queued_at, status, boarded_at) VALUES (:stationId, :queuedAt, :status, :boardedAt)";
+	$stationSql = "SELECT id, ( 6371 * acos( cos( radians( " . $user->lat . " ) ) * cos( radians( latitude ) ) * 
+			cos( radians( longitude ) - radians( " . $user->lng . ") ) + sin( radians( " . $user->lat . " ) ) * 
+			sin( radians( latitude ) ) ) ) AS distance FROM stations HAVING
+			distance < 0.05 ORDER BY distance LIMIT 1";
     try {
-        $db = getConnection();
-        $stmt = $db->prepare($sql);
-        $stmt->bindParam("stationId", $user->stationId);
-        $stmt->bindParam("queuedAt", $user->queuedAt);
-        $stmt->bindParam("status", $user->status);
-		$stmt->bindParam("boardedAt", $user->boardedAt);
-        $stmt->execute();
-        $user->id = $db->lastInsertId();
-        $db = null;
-        echo json_encode($user);
+		$db = getConnection();
+        $stmt = $db->query($stationSql);
+        $count = $stmt->rowCount();
+		$station = $stmt->fetch();
+		
+		if($count > 0){
+			$date = date('Y-m-d H:i:s');
+			$sql = "INSERT INTO users (station_id) VALUES (:stationId)";
+			$stmt = $db->prepare($sql);
+			$stmt->bindParam("stationId", $station["id"]);
+			$stmt->execute();
+			$user->id = $db->lastInsertId();
+			$db = null;
+			echo json_encode($user->id);
+		}
     } catch(PDOException $e) {
         echo '{"error":{"text":'. $e->getMessage() .'}}';
     }
@@ -100,17 +109,44 @@ function postUsers($request) {
 function insertBoardings($request) {
     $boarding = json_decode($request->getBody());
 	
-    $sql = "INSERT INTO boardings (user_id, bus_id, station_id, created_at) VALUES (:userId, :busId, :stationId, NOW())";
+	$stationSql = "SELECT id, ( 6371 * acos( cos( radians( " . $boarding->lat . " ) ) * cos( radians( latitude ) ) * 
+			cos( radians( longitude ) - radians( " . $boarding->lng . ") ) + sin( radians( " . $boarding->lat . " ) ) * 
+			sin( radians( latitude ) ) ) ) AS distance FROM stations HAVING
+			distance < 0.001 ORDER BY distance LIMIT 1";
     try {
-        $db = getConnection();
-        $stmt = $db->prepare($sql);
-        $stmt->bindParam("userId", $boarding->userId);
-        $stmt->bindParam("busId", $boarding->busId);
-        $stmt->bindParam("stationId", $boarding->stationId);
-        $stmt->execute();
-        $boarding->id = $db->lastInsertId();
-        $db = null;
-        echo json_encode($boarding);
+		$db = getConnection();
+        $stmt = $db->query($stationSql);
+        $count = $stmt->rowCount();
+		$station = $stmt->fetch();
+		
+		if($count > 0){
+			$busStmt = $db->query("select vacant_seats, id FROM bus where vacant_seats > 1 LIMIT 1");
+			$bus = $busStmt->fetch();
+			
+			$busSql = $db->prepare("UPDATE `bus` SET vacant_seats = :seat WHERE `id` = :busId");
+			$seat = $bus["vacant_seats"]-1;
+			$busSql->bindParam('seat', $seat);
+			$busSql->bindParam('busId', $bus["id"]);
+			$busSql->execute();
+			
+			$sql = "INSERT INTO boardings (user_id, bus_id, station_id, created_at) VALUES (:userId, :busId, :stationId, NOW())";
+			$stmt = $db->prepare($sql);
+			$stmt->bindParam("userId", $boarding->userId);
+			$stmt->bindParam("busId", $bus["id"]);
+			$stmt->bindParam("stationId", $station["id"]);
+			$stmt->execute();
+			$boarding->id = $db->lastInsertId();
+			
+			$status = 1;
+			$user = $db->prepare("UPDATE `users` SET status = :status, boarded_at = :boardedAt WHERE `id` = :userId");
+			$user->bindParam('status', $status);
+			$user->bindParam('boardedAt', $date);
+			$user->bindParam('userId', $boarding->userId);
+			$user->execute();
+			
+			$db = null;
+			echo json_encode("User has boarded!");
+		}
     } catch(PDOException $e) {
         echo '{"error":{"text":'. $e->getMessage() .'}}';
     }
